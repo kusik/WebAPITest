198e75946ae979029fa788b0cf71822f84633aba
Kusinczky Károly <hukku@coloplast.com>
2021-02-17 08:34:29 +0100

    Add project files.

---
+++ Controllers/DepartmentController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public DepartmentController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"select * from dbo.department";
+            DataTable dataTable = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+            SqlDataReader sqlDataReader;
+            using (SqlConnection sqlConnection = new SqlConnection(sqlDataSource))
+            {
+                sqlConnection.Open();
+                using SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+                sqlDataReader = sqlCommand.ExecuteReader();
+                dataTable.Load(sqlDataReader);
+                sqlDataReader.Close();
+                sqlConnection.Close();
+            }
+            return new JsonResult(dataTable);
+        }
+        [HttpPost]
+        public JsonResult Post()
+        {
+
+            string query = @"select * from dbo.department";
+            DataTable dataTable = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+            SqlDataReader sqlDataReader;
+            using (SqlConnection sqlConnection = new SqlConnection(sqlDataSource))
+            {
+                sqlConnection.Open ();
+                using SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+                sqlDataReader = sqlCommand.ExecuteReader();
+                dataTable.Load(sqlDataReader);
+                sqlDataReader.Close();
+                sqlConnection.Close();
+            }
+            return new JsonResult(dataTable);
+        }
+
+    }
+}
